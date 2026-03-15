717e300ff165d0942ce4d2d1b3bffe48c8d2978e
losha24 <losha24@gmail.com>
2026-03-15 20:02:45 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,8 @@
-const CACHE_NAME = 'chef-pro-v1.0.2';
-const ASSETS = ['index.html', 'styles.css', 'app.js', 'manifest.json', 'logo.png'];
-
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then((keys) => {
-    return Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)));
-  }));
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
-});
+// רישום Service Worker לעבודה באופליין
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./sw.js')
+      .then(reg => console.log('Service Worker Registered'))
+      .catch(err => console.log('Service Worker Failed', err));
+  });
+}
