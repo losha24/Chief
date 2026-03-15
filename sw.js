11cc7159ce5a77ac9c3bbaefecf7d0f17967eb59
losha24 <losha24@gmail.com>
2026-03-15 19:16:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,17 @@
-const CACHE_NAME = 'chef-pro-v1.0.0';
-const ASSETS = [
-  'index.html',
-  'styles.css',
-  'app.js',
-  'manifest.json',
-  'logo.png'
-];
+const CACHE_NAME = 'chef-pro-v1.0.2';
+const ASSETS = ['index.html', 'styles.css', 'app.js', 'manifest.json', 'logo.png'];
 
 self.addEventListener('install', (e) => {
   self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
-  );
+  e.waitUntil(caches.keys().then((keys) => {
+    return Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)));
+  }));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
