97e4865631ef73b55495a1427d4a01b8eee77d16
losha24 <losha24@gmail.com>
2026-03-16 14:10:58 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,40 @@
-const CACHE_NAME = 'chef-pro-v1.0.9';
-const assets = ['./', './index.html', './styles.css', './app.js', './manifest.json'];
+const CACHE_NAME = 'chef-pro-v1.1.0';
+const assets = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.json',
+  './logo.png'
+];
 
+// התקנה ושמירת קבצים במטמון
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('ChefPro: Caching assets');
+      return cache.addAll(assets);
+    })
+  );
 });
 
+// ניקוי גרסאות ישנות מהזיכרון
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// שליפת נתונים - תמיכה באופליין
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
