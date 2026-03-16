c3e690217cb00066b4959dbd8ac5e0679e5cd672
losha24 <losha24@gmail.com>
2026-03-16 19:44:50 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'chef-pro-v2.2.0';
+const CACHE_NAME = 'chef-pro-v2.2.1';
 const assets = ['./', './index.html', './styles.css', './app.js', './manifest.json', './logo.png'];
 
 self.addEventListener('install', e => {
