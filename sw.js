c33d836dce7327f537f616a37f0712fc5a385715
losha24 <losha24@gmail.com>
2026-03-16 14:38:58 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,16 @@
-const CACHE_NAME = 'chef-pro-v1.1.0';
-const assets = [
-  './',
-  './index.html',
-  './styles.css',
-  './app.js',
-  './manifest.json',
-  './logo.png'
-];
+const CACHE_NAME = 'chef-pro-v2.0.0';
+const assets = ['./', './index.html', './styles.css', './app.js', './manifest.json', './logo.png'];
 
-// התקנה ושמירת קבצים במטמון
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('ChefPro: Caching assets');
-      return cache.addAll(assets);
-    })
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-// ניקוי גרסאות ישנות מהזיכרון
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys
-        .filter(key => key !== CACHE_NAME)
-        .map(key => caches.delete(key))
-      );
-    })
-  );
+  e.waitUntil(caches.keys().then(keys => {
+    return Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
+  }));
 });
 
-// שליפת נתונים - תמיכה באופליין
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
